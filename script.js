320dd1b7da11eb2f72ea7dbb3a25ca08c4e27f02
simersbpl <simersbpl@gmail.com>
2023-04-17 12:53:41 +0530

    first commit full website with header footer and card animations

---
+++ script.js
@@ -0,0 +1,28 @@
+// gsap.to(".our-works", {duration: 2.5, x: 200, backgrounColor: "#ccc", scale: 1.5, ease: "ease",
+// repeat: -1,
+// delay: 1,
+// yoyo: true,
+// rotate:("1deg"),
+// fontWeight: 700,
+// margin: 2,
+// padding: 2
+// })
+
+
+// gsap.from('.our-works', {
+//   duration: 2.5,
+//   delay: 3.5,
+//   x: 1500,
+//   scale: 3,
+//   color: green,
+// })
+
+
+// gsap.from("#main-swipper", { 
+//     duration: 1.5,
+//     // backgroundColor: '#8d3dae',
+//     // rotate:("360deg"),
+//     x: 2200,
+//     y: 1000,
+//     delay: 1.5
+//   });
